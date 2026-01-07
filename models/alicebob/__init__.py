b91cb9d34df7cb2b4c82c4307f294d58b3109049
Pallandos <commit@pallandos.dev>
2026-01-07 09:36:53 +0100

    add all 3 alice and bob

---
+++ models/alicebob/__init__.py
@@ -0,0 +1,7 @@
+"""
+Alice and Bob Models Package
+"""
+
+from .ab1 import Ab1
+from .ab2 import Ab2
+from .ab3 import Ab3
---
+++ models/alicebob/ab3.py
@@ -0,0 +1,39 @@
+from struct.graph import RootedGraph
+
+class Ab3(RootedGraph):
+    """Alice and Bob Model 3
+    """
+    
+    def __init__(self):
+        print("AB3 model initialized") 
+        print("States are represented as (Alice_state, Bob_state, Alice_flag, Bob_flag)")
+    
+    def roots(self) -> list:
+        return [("I", "I", "down", "down")]
+    
+    def neighbors(self, vertex) -> list:
+        alice_state, bob_state, alice_flag, bob_flag = vertex
+        neighbors = []
+        
+        if alice_state == "I":
+            neighbors.append(("CS", bob_state, "up", bob_flag))
+        
+        if bob_state == "I":
+            neighbors.append((alice_state, "CS", alice_flag, "up"))
+        
+        if alice_state == "W" and bob_flag == "down":
+            neighbors.append(("CS", bob_state, "up", "down"))
+        
+        if bob_state == "W" and alice_flag == "down":
+            neighbors.append((alice_state, "CS", "down", "up"))
+        
+        if bob_state == "W" and alice_flag == "up":
+            neighbors.append((alice_state, "I", "up", "down"))
+        
+        if alice_state == "CS":
+            neighbors.append(("I", bob_state, "down", bob_flag))
+        
+        if bob_state == "CS":
+            neighbors.append((alice_state, "I", alice_flag, "down"))
+                    
+        return neighbors
