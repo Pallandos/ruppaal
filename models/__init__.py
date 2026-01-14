18f832e3befaa609dde5924809a2b1a1d0e36d23
Pallandos <commit@pallandos.dev>
2026-01-14 09:42:18 +0100

    chor: minor

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-""" This module initializes the models package.
+""" This module initializes the models package. Models are implementations of abstract and general structures from the struct package.
 
-Including : alice and bob models.
+Including : alice and bob models, hanoi models, LS2RG.
 """
