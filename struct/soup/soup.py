33514578f33c3b5f4c811d10202d0637cc4cf475
Pallandos <commit@pallandos.dev>
2026-01-23 08:47:44 +0100

    improve soup

--- struct/soup/soup.py
+++ struct/soup/soup.py
@@ -5,6 +5,15 @@ class Soup :
     def __init__(self, pieces, init_value):
         self.pieces = pieces
         self.init_value = init_value
+        
+    def extend(self, b):
+        if isinstance(b, Piece):
+            self.pieces.append(b)
+        else:
+            self.pieces.extend(b)
+    
+    def add(self, name, guard, generator):
+        self.extend(Piece(name, guard, generator))
 
 class Piece:
     """
@@ -16,3 +25,8 @@ class Piece:
         self.effet = effet
         self.garde = garde
         self.nom = nom
+    
+    def __eq__(self, other):
+        if not isinstance(other, Piece):
+            return False
+        return self.nom == other.nom and self.garde == other.garde and self.effet == other.effet
