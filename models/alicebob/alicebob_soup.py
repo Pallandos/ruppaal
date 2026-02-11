0f384fd8f7b04911a24a6dff4b753780b077ef7b
jeremlechat <lacuisine@tutanota.com>
2026-02-11 16:33:13 +0100

    deplace AliceBobISoup class from alicebob_soup.py
    deplace AliceBobISoup class and its associated logic.

--- models/alicebob/alicebob_soup.py
+++ models/alicebob/alicebob_soup.py
@@ -39,33 +39,4 @@ class AliceBobSoup(Soup):
         
         super().__init__(pieces, init_state)
 
-class AliceBobISoup(Isoup):
-    def __init__(self, soup_instance):
-        # The ISoup takes the state of the soup as input for its guards
-        # We initialize it with the same initial value as the soup
-        init_value = soup_instance.init_value
-        
-        ipieces = []
-        
-        # Characteristic: Alice Critical
-        ipieces.append(Ipiece(
-            lambda s: s, # Identity effect
-            lambda s: s[0] == 1,
-            "Alice_is_Critical"
-        ))
-        
-        # Characteristic: Bob Critical
-        ipieces.append(Ipiece(
-            lambda s: s,
-            lambda s: s[1] == 1,
-            "Bob_is_Critical"
-        ))
-        
-        # Characteristic: Exclusion Violation (Collision)
-        ipieces.append(Ipiece(
-            lambda s: s,
-            lambda s: s[0] == 1 and s[1] == 1,
-            "Collision_Occurred"
-        ))
-        
-        super().__init__(ipieces, init_value)
+
